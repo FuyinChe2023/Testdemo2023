af9962296b6e7f94cc982ed6fc53fb9d72ed7e91
FuyinChe2023 <134092209+FuyinChe2023@users.noreply.github.com>
2023-05-26 12:42:23 -0400

    Update TempDemos23FriAM.java
    add comment for fetch and merge

--- src/main/java/com/mycompany/tempdemos23friam/TempDemos23FriAM.java
+++ src/main/java/com/mycompany/tempdemos23friam/TempDemos23FriAM.java
@@ -13,6 +13,8 @@ public class TempDemos23FriAM {
     //comment added
     
     //add comment @ 12:41 May 26, 2023
+    
+    //comment for fetch and merge
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
