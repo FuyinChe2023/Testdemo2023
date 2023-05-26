fd631241977670f0956d57ac80101d87655541f5
Fuyin2023 <chefu@sheridancollege.ca>
2023-05-26 12:23:42 -0400

    add comment

---
+++ src/main/java/com/mycompany/tempdemos23friam/TempDemos23FriAM.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tempdemos23friam;
+
+/**
+ *
+ * @author Alvin Che
+ */
+public class TempDemos23FriAM {
+    
+    //comment added
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
